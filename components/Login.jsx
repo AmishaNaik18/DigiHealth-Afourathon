cf220087d333bf57235d17a4d5dd87499481703d
Ema B <gecproject2021@gmail.com>
2022-06-30 16:29:28 +0530

    modified login

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { React, useState, Fragment } from "react"
 
 const Login = () => {
+
+  const [formData,setForm] = useState({
+    username:'',
+    password:''
+  })
+
+  const {username,password} = formData
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    // Login action function
+  }
+
+  const onChange = (e) => { setForm({...formData, [e.name]:e.value })}
+
   return (
+    <Fragment>
+      <form onSubmit={onSubmit}>
     <div className="lg:flex flex-row">
       <img src="login/d3.jpg" className="h-screen" />
       <div className="flex flex-col items-center w-full py-10">
@@ -9,14 +26,15 @@ const Login = () => {
         <text className="text-sm font-semibold">Lets get you logged in</text>
         <div className="h-10 w-[70%] bg-[#DCDCDC] mt-8 rounded-sm flex flex-row">
           <img src="login/stethoscope.png" className="h-10" />
-          <input className="h-10 w-full bg-[#DCDCDC]" name="userid" />
+          <input type="username" className="h-10 w-full bg-[#DCDCDC]" placeholder="Username" name="username" value={username} onChange={(e)=>{onChange(e.target)}} />
         </div>
         <div className="h-10 w-[70%] bg-[#DCDCDC] mt-6 rounded-sm flex flex-row">
           <img src="login/key.jpg" className="h-10 w-10" />
           <input
-            className="h-10 w-full bg-[#DCDCDC]"
-            name="password"
             type="password"
+            placeholder="Password"
+            name="password"
+            value={password} onChange={(e)=>{onChange(e.target)}}
           />
         </div>
         <button
@@ -27,6 +45,8 @@ const Login = () => {
         </button>
       </div>
     </div>
+    </form>
+    </Fragment>
   );
 }
 
