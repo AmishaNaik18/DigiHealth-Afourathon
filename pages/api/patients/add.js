3391b679b0aa2fc7e047a65da27b665cd9b2fe28
Ema B <gecproject2021@gmail.com>
2022-07-02 20:29:16 +0530

    stashed changes

--- pages/api/patients/add.js
+++ pages/api/patients/add.js
@@ -1,13 +1,13 @@
-import Patient from "../../../models/Patient";
-import connectMongo from "../../../utils/connectMongo";
+import Patient from '../../../models/Patient';
+import connectMongo from '../../../utils/connectMongo';
 
-export default async function addPatient (req,res) {
-    try {
-        await connectMongo();
-        const patient = await Patient.create(req.body);
-        console.log(patient);
-        res.status(200).json("Patient Created");
-    } catch (error) {
-        console.log("Patient could not be added ", error);
-    }
+export default async function addPatient(req, res) {
+	try {
+		await connectMongo();
+		const patient = await Patient.create(req.body);
+		console.log(patient);
+		res.status(200).json('Patient Created');
+	} catch (error) {
+		console.log('Patient could not be added ', error);
+	}
 }
